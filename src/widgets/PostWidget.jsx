08f3577e9db6ff60873cf98802cb9449d360e587
Jaron-S <jaronschoor@gmail.com>
2023-05-09 15:55:47 -0600

    adjusted post min width

--- src/widgets/PostWidget.jsx
+++ src/widgets/PostWidget.jsx
@@ -2,18 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Divider, Typography } from "@mui/material";
 import WidgetWrapper from "../components/WidgetWrapper";
+import { Box } from "@mui/material";
 
 const PostWidget = ({ post }) => {
   return post ? (
     <WidgetWrapper>
-      <Typography gutterBottom variant="h5" component="div">
-        {post.title}
-      </Typography>
-      <Divider />
-      <Typography color="text.secondary">{post.description}</Typography>
-      <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
-        <Link to={`/profile/${post.userId}`}>Profile</Link>
-      </Typography>
+      <Box maxWidth="50rem">
+        <Typography gutterBottom variant="h5" component="div">
+          {post.title}
+        </Typography>
+        <Divider />
+        <Typography color="text.secondary">{post.description}</Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ mt: 2 }}>
+          <Link to={`/profile/${post.userId}`}>Profile</Link>
+        </Typography>
+      </Box>
     </WidgetWrapper>
   ) : (
     <></>
