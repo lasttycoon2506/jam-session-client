cda6160655157008f2685349c11d7e72facd4fc9
Jaron-S <jaronschoor@gmail.com>
2023-05-16 14:15:40 -0600

    fixed comment

--- src/widgets/Newsfeed.jsx
+++ src/widgets/Newsfeed.jsx
@@ -8,19 +8,17 @@ const Newsfeed = () => {
   const posts = useSelector((state) => state.posts); // used to track the value of state.posts
 
   // Send a request to the server for the posts and store in state
-  //    note: I can skip specifying the fetch request type (GET, POST, etc.)
-  //          because there's only one route at /posts
   const getPosts = async () => {
     fetch("https://jam-session.onrender.com/posts")
-      .then((response) => response.json())                  // 1. parse data
-      .then((data) => dispatch(setPosts({ posts: data })))  // 2. store data in state
-      .catch((error) => console.error(error));              // 3. error logging
+      .then((response) => response.json()) // 1. parse data
+      .then((data) => dispatch(setPosts({ posts: data }))) // 2. store data in state
+      .catch((error) => console.error(error)); // 3. error logging
   };
 
   // Run getPosts on page load
   useEffect(() => {
     getPosts(); // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);         // ^ this just disables an unecessary linting error
+  }, []); // ^ this just disables an unecessary linting error
 
   return (
     <div>
