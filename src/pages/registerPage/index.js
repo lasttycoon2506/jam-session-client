87b069cafb8bdfa57223172602c22c980be868d5
Gabriel Valenzuela <mgvalenzu@gmail.com>
2023-05-20 18:02:27 -0700

    Add some validation to register page form.

--- src/pages/registerPage/index.js
+++ src/pages/registerPage/index.js
@@ -1,6 +1,7 @@
 import WidgetWrapper from "../../components/WidgetWrapper";
 import React, { useState, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 // import { isEmail } from "validator";
 
 const initialRegisterData = {
@@ -8,16 +9,18 @@ const initialRegisterData = {
   email: "",
   password: "",
   location: "",
-  experience: "",
+  bandExperience: "",
   genres: "",
   availability: "",
-  instruments: "",
+  instrumentName: "",
+  yearsExperience: "",
+  proficiency: "",
 };
 
 const RegisterPage = () => {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState(initialRegisterData);
-  const API_URL = "https://jam-session.onrender.com/auth/register"; // might be some issue here with cors
+  const API_URL = "https://jam-session.onrender.com/auth/register"; 
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,12 +31,16 @@ const RegisterPage = () => {
     });
   };
 
-  //TODO: Validation that user has input required fields - also add * for required fields
-
-  //TODO: build instrument json object
-
   const register = async (event) => {
     event.preventDefault();
+
+    //Build instruments json object
+    const instruments = {
+      instrumentName: formData.instrumentName,
+      yearsExperience: formData.yearsExperience,
+      proficiency: formData.proficiency,
+    }
+
     const newUser = {
       name: formData.name,
       email: formData.email,
@@ -42,7 +49,7 @@ const RegisterPage = () => {
       bandExperience: formData.bandExperience,
       genres: formData.genres,
       availability: formData.availability,
-      instruments: formData.instruments,
+      instruments: instruments,
     };
 
     try {
@@ -61,142 +68,159 @@ const RegisterPage = () => {
       const responseData = await response.json();
       console.log("POST request successful");
       console.log(responseData);
-      
+      window.location = `/login`;
     } catch (error) {
       console.error("Error with POST request:", error);
     }
   };
-
 
   return (
     <div>
       <WidgetWrapper>Register Now to Start Jamming</WidgetWrapper>
       <WidgetWrapper>
         <div className="registerForm">
-          <div>
-            <label htmlFor="">Name</label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Rodrigo00"
-              value={formData.name}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="">Email</label>
-            <input
-              type="text"
-              placeholder="email@jam.sesh"
-              name="email"
-              value={formData.email}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="">Password</label>
-            <input
-              type="text"
-              name="password"
-              placeholder="8-16 Characters"
-              value={formData.password}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="">Location</label>
-            <input
-              type="text"
-              name="location"
-              placeholder="Los Angeles, Ca"
-              value={formData.location}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="">Band Experience</label>
-            <input
-              type="text"
-              name="bandExperience"
-              placeholder="3 Years"
-              value={formData.bandExperience}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="">Genres</label>
-            <input
-              type="text"
-              name="genres"
-              placeholder="Heavy Metal, Slowcore, City Pop"
-              value={formData.type}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            <label htmlFor="Availability ">Availability</label>
-            <input
-              type="text"
-              name="availability"
-              placeholder="M-F 3Pm"
-              value={formData.type}
-              onChange={handleInputChange}
-            ></input>
-          </div>
-
-          <div>
-            -- Primary Instrument(you can add more later) --
+          <form onSubmit={register}>
             <div>
-              <label htmlFor="">Instrument Name</label>
-              <input name="instrumentName" type="text" placeholder=""></input>
-            </div>
-            <div>
-              <label htmlFor="">Years of Experice</label>
-              <select
-                name="yearsExperience"
+              *<label htmlFor="">Name</label>
+              <input
                 type="text"
-                placeholder="0"
+                name="name"
+                placeholder="Rodrigo00"
+                value={formData.name}
+                onChange={handleInputChange}
+                required
+                maxLength="32"
+              ></input>
+            </div>
+
+            <div>
+              *<label htmlFor="">Email</label>
+              <input
+                type="text"
+                placeholder="email@jam.sesh"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
+                required
+                pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                title="ex: newuser@jamsesh.com"
+              ></input>
+            </div>
+
+            <div>
+              *<label htmlFor="">Password</label>
+              <input
+                type="text"
+                name="password"
+                placeholder="8-16 Characters"
+                value={formData.password}
+                onChange={handleInputChange}
+                required
+                min="8"
+                max="16"
+                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,16}"
+                title="Must contain at least 1 numer, 1 upper case letter, 1 lowercase letter"
+              ></input>
+            </div>
+
+            <div>
+              *<label htmlFor="">Location</label>
+              <input
+                type="text"
+                name="location"
+                placeholder="Los Angeles, Ca"
+                value={formData.location}
+                onChange={handleInputChange}
+                required
+              ></input>
+            </div>
+
+            <div>
+              <label htmlFor="">Band Experience</label>
+              <input
+                type="text"
+                name="bandExperience"
+                placeholder="3 Years"
+                value={formData.bandExperience}
+                onChange={handleInputChange}
+              ></input>
+            </div>
+
+            <div>
+              <label htmlFor="">Genres</label>
+              <input
+                type="text"
+                name="genres"
+                placeholder="Heavy Metal, Slowcore, City Pop"
                 value={formData.type}
                 onChange={handleInputChange}
-              >
-                <option value="0">0</option>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10+</option>
-              </select>
+              ></input>
             </div>
+
             <div>
-              <label htmlFor="">Proficiency</label>
-              <select
-                name="proficiency"
+              <label htmlFor="Availability ">Availability</label>
+              <input
                 type="text"
-                placeholder=""
-                value={formData.type}
+                name="availability"
+                placeholder="M-F 3Pm"
+                value={formData.availability}
                 onChange={handleInputChange}
-              >
-                <option value="Novice">Novice</option>
-                <option value="Intermediate">Intermediate</option>
-                <option value="Advanced">Advanced</option>
-                <option value="Expert">Expert</option>
-              </select>
+              ></input>
             </div>
-          </div>
-          <button className="mt-3" onClick={register}>
-            Lets Jam!
-          </button>
+
+            <div>
+              -- Primary Instrument(you can add more later) --
+              <div>
+                <label htmlFor="">Instrument Name</label>
+                <input
+                  name="instrumentName"
+                  type="text"
+                  placeholder=""
+                  value={formData.instrumentName}
+                  onChange={handleInputChange}
+                ></input>
+              </div>
+              <div>
+                <label htmlFor="">Years of Experice</label>
+                <select
+                  name="yearsExperience"
+                  type="text"
+                  placeholder="0"
+                  value={formData.yearsExperience}
+                  onChange={handleInputChange}
+                >
+                  <option value="0">0</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10+</option>
+                </select>
+              </div>
+              <div>
+                <label htmlFor="">Proficiency</label>
+                <select
+                  name="proficiency"
+                  type="text"
+                  placeholder=""
+                  value={formData.proficiency}
+                  onChange={handleInputChange}
+                >
+                  <option value="Novice">Novice</option>
+                  <option value="Intermediate">Intermediate</option>
+                  <option value="Advanced">Advanced</option>
+                  <option value="Expert">Expert</option>
+                </select>
+              </div>
+            </div>
+            <button className="mt-3">Lets Jam!</button>
+            <div>* - required</div>
+          </form>
         </div>
       </WidgetWrapper>
     </div>
