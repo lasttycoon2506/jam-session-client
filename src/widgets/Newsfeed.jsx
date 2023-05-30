e7889f8161c0f5947bd6152256e0e7dba99bc00a
Jaron-S <jaronschoor@gmail.com>
2023-05-30 13:52:19 -0600

    Added token authorization to newsfeed

--- src/widgets/Newsfeed.jsx
+++ src/widgets/Newsfeed.jsx
@@ -7,15 +7,20 @@ import { useParams } from "react-router-dom";
 const Newsfeed = () => {
   const dispatch = useDispatch(); // used to store values in state (see below)
   const posts = useSelector((state) => state.posts); // used to track the value of state.posts
+  const token = useSelector((state) => state.token);
   let API_URL = "https://jam-session.onrender.com/posts";
 
-  //Designate url for user/non-user newsfeed
+  // Designate url for user/non-user newsfeed
   let { userId } = useParams();
-  if (userId !== undefined) API_URL += `/${userId}` 
+  if (userId !== undefined) API_URL += `/${userId}`;
 
   // Send a request to the server for the posts and store in state
   const getPosts = async () => {
-    fetch(API_URL)
+    fetch(API_URL, {
+      headers: {
+        Authorization: token,
+      },
+    })
       .then((response) => response.json())                  // 1. parse data
       .then((data) => dispatch(setPosts({ posts: data })))  // 2. store data in state
       .catch((error) => console.error(error));              // 3. error logging
