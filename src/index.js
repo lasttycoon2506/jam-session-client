04f1442336ad68c8d5ad2ad2c0964856b520e1c5
Jaron-S <jaronschoor@gmail.com>
2023-05-30 19:20:43 -0600

    added persistent state

--- src/index.js
+++ src/index.js
@@ -3,18 +3,26 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 import rootReducer from "./state";
 
 // Redux setup
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
 });
+const persistor = persistStore(store);
 
 const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
